73996b1bac4dfec41f8edf2721c448040fc25eb9
Doun <liushi2008wh@163.com>
2017-10-09 23:51:57 +0800

    提交了一些乱代码

---
+++ Chapter2IPC/app/src/main/java/com/example/doun/chapter2ipc/MyApplication.java
@@ -0,0 +1,33 @@
+package com.example.doun.chapter2ipc;
+
+//import com.example.doun.chapter2ipc.binderpool.BinderPool;
+import com.example.doun.chapter2ipc.utils.MyUtils;
+
+import android.app.Application;
+import android.os.Process;
+import android.util.Log;
+
+public class MyApplication extends Application {
+
+    private static final String TAG = "MyApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        String processName = MyUtils.getProcessName(getApplicationContext(),
+                Process.myPid());
+        Log.d(TAG, "application start, process name:" + processName);
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                doWorkInBackground();
+            }
+        }).start();
+    }
+
+    private void doWorkInBackground() {
+        // init binder pool
+//        BinderPool.getInsance(getApplicationContext());
+    }
+}
